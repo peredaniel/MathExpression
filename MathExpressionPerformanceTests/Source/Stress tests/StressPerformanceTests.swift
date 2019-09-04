e23363fcb8e54c38a1e926152608a9ded0f5b971
Pere Daniel Prieto <pere@wearemobilefirst.com>
2019-09-04 14:29:36 +0200

    * Removed unnecessary duplicated files in tests. Moved remaining ones to a single folder 'MathExpressionTestHelpers', and assigned targets accordingly. * Implemented RandomExpressionGenerator class to randomly generate mathematical expressions by combining a fixed set of pre-selected expressions.

--- MathExpressionPerformanceTests/Source/Stress tests/StressPerformanceTests.swift
+++ MathExpressionPerformanceTests/Source/Stress tests/StressPerformanceTests.swift
@@ -4,6 +4,18 @@
 import XCTest
 
 class StressPerformanceTests: XCTestCase {
+    func testPerformanceFullParenthesesSingleValue() {
+        let numberOfParentheses = 500
+        var expression = String(repeating: Character(MathBrackets.parenthesis.opening), count: numberOfParentheses)
+        expression += "10"
+        expression += String(repeating: Character(MathBrackets.parenthesis.closing), count: numberOfParentheses)
+
+        measure {
+            let mathExpression = try! MathExpression(expression)
+            _ = mathExpression.evaluate()
+        }
+    }
+
     func testPerformanceFullParenthesesSumAndProduct() {
         let numberOfParentheses = 500
         var expression = ""
@@ -11,25 +23,17 @@ class StressPerformanceTests: XCTestCase {
             expression += "(\(Int.random(in: -100...100)) + \(Int.random(in: -100...100)) * "
         }
         expression += "(\(Int.random(in: -100...100)) + \(Int.random(in: -100...100))"
-        for _ in 0...numberOfParentheses {
-            expression += ")"
-        }
+        expression += String(repeating: Character(MathBrackets.parenthesis.closing), count: numberOfParentheses + 1)
         measure {
             let mathExpression = try! MathExpression(expression)
             _ = mathExpression.evaluate()
         }
     }
 
-    func testPerformanceFullParentheses() {
-        let numberOfParentheses = 500
-        var expression = ""
-        for _ in 0..<numberOfParentheses {
-            expression += "("
-        }
-        expression += "10"
-        for _ in 0..<numberOfParentheses {
-            expression += ")"
-        }
+    func testPerformanceRandomExpression() {
+        let randomGenerator = RandomExpressionGenerator()
+        let expression = randomGenerator.generateRandomExpression(combining: 500)
+
         measure {
             let mathExpression = try! MathExpression(expression)
             _ = mathExpression.evaluate()
